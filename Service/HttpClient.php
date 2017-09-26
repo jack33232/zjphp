b2c9af902cf7361086976dae9d7b8dd072013639
jack33232 <jack33232@gmail.com>
2017-09-26 09:20:16 +0800

    - Update the HttpClient Service

--- Service/HttpClient.php
+++ Service/HttpClient.php
@@ -4,11 +4,29 @@ namespace ZJPHP\Service;
 use ZJPHP\Base\ZJPHP;
 use ZJPHP\Base\Component;
 use GuzzleHttp\Client;
+use GuzzleHttp\Cookie\CookieJar;
 
 class HttpClient extends Component
 {
+    protected $globalConfig = [
+        'connection_timeout' => 0,
+        'timeout' => 0,
+        'verify' => true
+    ];
+
     public function instance(array $config = [])
     {
+        $config = $config + $this->globalConfig;
         return new Client($config);
+    }
+
+    public function getCookieJar()
+    {
+        return new CookieJar();
+    }
+
+    public function setGlobalConfig(array $config)
+    {
+        $this->globalConfig = $config + $this->globalConfig;
     }
 }
